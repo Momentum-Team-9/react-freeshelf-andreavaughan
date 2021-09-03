18562594620f69d376b6551253c0eaf5f66d58d5
andreavaughan <amvaughan16@gmail.com>
2021-09-03 15:58:46 -0400

    book title rendering

--- freeshelf/src/app.js
+++ freeshelf/src/app.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { books as data } from './data'
 
+export const Book = ({ title }) => {
+    const [books, viewBooks] = useState(data)
+    
+    return (
+        <div>
+            <header>
+                <h2>{ title }</h2>
+            </header>
+        </div>
+    )
+}
+
+
 export const App = () => {
-    return
+    const [books, viewBooks] = useState(data)
+
+    return (
+        <>
+            <div>
+                <h1>Books!</h1>
+                <div>
+                    {books.map((book, idx) => {
+                        return <Book key={idx} title={book.title} />
+                    })}
+                </div>
+            </div>
+        </>
+    )
 }
