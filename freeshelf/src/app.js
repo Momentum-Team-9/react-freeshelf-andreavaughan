e473e426d050a12259c6aa70e866962ac9362e59
andreavaughan <amvaughan16@gmail.com>
2021-09-03 19:57:05 -0400

    added basic book rendering

--- freeshelf/src/app.js
+++ freeshelf/src/app.js
@@ -3,17 +3,21 @@ import { books as data } from './data'
 
 
 export const Book = ({ book }) => {
-    
+    console.log(book.coverImageUrl)
+
+
     return (
-        <div class="col">
+        <div className="col">
             <div className="card mb-3" style={{maxWidth: 540}}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src="#" className="img-fluid rounded-start"></img>
+                        <img src={ book.coverImageUrl } className="img-fluid rounded-start"/>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
                             <h4 className="card-title">{ book.title }</h4>
+                            <h5 className="card-text">{ book.author }</h5>
+                            <p className="card-text">{ book.shortDescription }</p>
                         </div>
                     </div>
                 </div>
